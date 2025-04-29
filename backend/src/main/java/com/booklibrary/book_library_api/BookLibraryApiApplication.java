7ac4bf199a7b8ecddfff3a9a4d10e17642f1c923
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-28 21:02:43 -0700

    testing Java get API

--- backend/src/main/java/com/booklibrary/book_library_api/BookLibraryApiApplication.java
+++ backend/src/main/java/com/booklibrary/book_library_api/BookLibraryApiApplication.java
@@ -2,12 +2,20 @@ package com.booklibrary.book_library_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class})
+@RestController
 public class BookLibraryApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookLibraryApiApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String index(){
+		return "Hello world from Spring boot!";
 	}
 
 }
