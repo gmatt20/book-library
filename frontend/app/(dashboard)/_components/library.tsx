cebd46573336e8a419be416d825538527e6a463b
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-28 21:43:51 -0700

    imported custom theme colors

--- frontend/app/(dashboard)/_components/library.tsx
+++ frontend/app/(dashboard)/_components/library.tsx
@@ -2,9 +2,9 @@ import Book from "./book";
 
 const Library = () => {
   return (
-    <div className="w-full h-full">
-      <p>I am a library!</p>
-      <div className="grid grid-cols-3 grid-flow-row items-center justify-center">
+    <div className="flex flex-col items-center justify-center bg-neutral-700 m-5 p-5">
+      <p>Matthew&apos;s Library</p>
+      <div className="grid grid-cols-3 grid-flow-row">
         <Book />
         <Book />
         <Book />
--- frontend/app/(dashboard)/_components/navbar.tsx
+++ frontend/app/(dashboard)/_components/navbar.tsx
@@ -1,6 +1,6 @@
 const Navbar = () => {
   return (
-    <div className="flex items-center justify-between p-5">
+    <div className="flex items-center justify-between p-5 bg-dark-a0">
       <p>I am a navbar!</p>
       <ul className="flex items-center justify-center gap-3">
         <li>Add a Book</li>
