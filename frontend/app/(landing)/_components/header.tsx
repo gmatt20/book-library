51934a863c4add7b072ac4d78333fd807be68696
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-28 22:06:28 -0700

    improved landing UI a bit

--- frontend/app/(landing)/_components/header.tsx
+++ frontend/app/(landing)/_components/header.tsx
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 const Header = () => {
   return (
-    <div className="h-full w-full flex items-center justify-center flex-col">
+    <div className="h-full w-full flex items-center justify-center flex-col gap-5">
       <p>Welcome to Book Libary!</p>
-      <Link href="/dashboard">Go Add A Book</Link>
+      <Link href="/dashboard">
+        <Button variant="secondary">Try it out</Button>
+      </Link>
     </div>
   );
 };
