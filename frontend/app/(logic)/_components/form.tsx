850938e4f5337a7941eb1720f2ab7b90a7b49a11
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-29 21:56:10 -0700

    improved form UI

--- frontend/app/(logic)/_components/form.tsx
+++ frontend/app/(logic)/_components/form.tsx
@@ -1,17 +1,35 @@
+import { Button } from "@/components/ui/button";
+
 const Form = () => {
   return (
     <>
-      <form>
-        <label>Book Title:</label>
-        <input type="text" required name="title" />
-        <label>Book Author:</label>
-        <input type="text" required name="author" />
-        <label>Optional Image:</label>
-        <input type="file" required name="cover" accept="image/png, image/jpeg" />
+      <form className="flex flex-col items-center justify-center m-5 p-5 border-2 border-surface-a10 rounded-2xl">
+        <p className="mb-5">Add a book to your library</p>
+        <fieldset className="flex flex-col items-center justify-center">
+          <label>Book Title:</label>
+          <input
+            className="my-4 p-2 border-2 border-surface-a10 rounded-md"
+            type="text"
+            required
+            name="title"
+          />
+          <label>Book Author:</label>
+          <input
+            className="my-4 p-2 border-2 border-surface-a10 rounded-md"
+            type="text"
+            required
+            name="author"
+          />
+          <Button variant="secondary" className="cursor-pointer">
+            Upload Optional Image Cover
+          </Button>
+          <Button variant="secondary" className="mt-10 cursor-pointer bg-green-600">
+            Add your book
+          </Button>
+        </fieldset>
       </form>
-      <p>Form goes here</p>
     </>
   );
-}
+};
 
 export default Form;
