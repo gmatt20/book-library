991b681988fb4f4283704012118a7b23ad3536f2
Matthew Garcia <gmatthew2765@gmail.com>
2025-04-30 20:37:28 -0700

    created post and get books routes

--- backend/src/main/java/com/booklibrary/book_library_api/LibraryRestController.java
+++ backend/src/main/java/com/booklibrary/book_library_api/LibraryRestController.java
@@ -1,12 +1,25 @@
 package com.booklibrary.book_library_api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class LibraryRestController {
-    @GetMapping("/add")
-    public String index(){
-        return "You have reached the add route!";
+    @Autowired
+
+    private BookRepository bookRepository;
+
+    @PostMapping(path="/add")
+    public @ResponseBody String addNewBook(@RequestParam String title, @RequestParam String author){
+        Book newBook = new Book();
+        newBook.setTitle(title);
+        newBook.setAuthor(author);
+        bookRepository.save(newBook);
+        return "Saved!";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<Book> getAllBooks(){
+        return bookRepository.findAll();
     }
 }
