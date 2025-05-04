ddef670e4ab06fe551aaeb1c6532b9d2c04923fd
Matthew Garcia <gmatthew2765@gmail.com>
2025-05-03 23:05:44 -0700

    created loading book component

--- frontend/app/(dashboard)/_components/book.tsx
+++ frontend/app/(dashboard)/_components/book.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import DropdownMenuComponent from "./dropdownmenucomponent";
 import { deleteBook } from "@/lib/deleteBook";
 import EditModal from "./editmodal";
+import LoadingBook from "./loadingBook";
 
 const Book = () => {
   const [books, setBooks] = useState([]);
@@ -11,8 +12,10 @@ const Book = () => {
     title: string;
     author: string;
   } | null>(null);
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
+    setLoading(true);
     async function getAllBooks() {
       try {
         const response = await fetch("http://localhost:8080/library/book/all", {
@@ -29,6 +32,7 @@ const Book = () => {
       }
     }
     getAllBooks();
+    setLoading(false);
   }, []);
 
   const handleDelete = async (bookId: number) => {
@@ -53,7 +57,8 @@ const Book = () => {
       {books.map((book) => (
         <div
           key={book.id}
-          className="bg-surface-a10 w-72 min-h-[100%] max-w-[80%] min-w-[50%] rounded overflow-hidden p-3 h-fit">
+          className="bg-surface-a10 w-72 min-h-[100%] max-w-[80%] min-w-[50%] rounded overflow-hidden p-3 h-fit 
+          border-2 border-transparent hover:border-2 hover:border-primary-a0 transition duration-400 ease-in-out">
           <span className="flex justify-end mb-2 cursor-pointer">
             <DropdownMenuComponent
               bookId={book.id}
@@ -77,6 +82,12 @@ const Book = () => {
           bookAuthor={editBook.author}
           onClose={() => setEditBook(null)}
         />
+      )}
+      {isLoading && (
+        <>
+          <LoadingBook />
+          <LoadingBook />
+        </>
       )}
     </>
   );
