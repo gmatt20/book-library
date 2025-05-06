ef5d8f8fa415cbf50f9e87c7fbdb2dd556c1f301
Matthew Garcia <gmatthew2765@gmail.com>
2025-05-05 22:20:59 -0700

    bug: fix html background color gap

--- frontend/app/(dashboard)/_components/library.tsx
+++ frontend/app/(dashboard)/_components/library.tsx
@@ -3,12 +3,12 @@ import Book from "./book";
 const Library = () => {
     return (
         <div
-            className="flex flex-col items-center justify-center md:m-10 md:border-2 md:border-surface-a10 rounded-2xl ">
+            className="flex flex-col items-center justify-center md:m-10 md:border-2 md:border-surface-a10 rounded-2xl">
             <p className="text-primary-a50 text-2xl font-bold uppercase border-b-2 border-primary-a50 w-full text-center py-3">
                 Matthew&apos;s Library
             </p>
             <div
-                className="grid grid-cols-1 md:grid-cols-2 grid-flow-row place-items-center gap-10 lg:p-10 py-10 ">
+                className="grid grid-cols-1 md:grid-cols-2 grid-flow-row place-items-center gap-10 lg:p-10 py-10">
                 <Book/>
             </div>
         </div>
--- frontend/app/(dashboard)/dashboard/page.tsx
+++ frontend/app/(dashboard)/dashboard/page.tsx
@@ -1,11 +1,11 @@
 import Library from "../_components/library";
 
 const Dashboard = () => {
-  return (
-    <div className="bg-surface-a0 relative">
-      <Library />
-    </div>
-  );
+    return (
+        <div className="bg-surface-a0 relative">
+            <Library/>
+        </div>
+    );
 };
 
 export default Dashboard;
