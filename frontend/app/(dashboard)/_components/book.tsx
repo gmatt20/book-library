23d0d97189a20186be8fb9a55c7fa245eab48620
Matthew Garcia <gmatthew2765@gmail.com>
2025-05-05 22:39:23 -0700

    mobile responsiveness: made book cards responsive and fluid

--- frontend/app/(dashboard)/_components/book.tsx
+++ frontend/app/(dashboard)/_components/book.tsx
@@ -1,96 +1,98 @@
 "use client";
-import { useState, useEffect } from "react";
+import {useEffect, useState} from "react";
 import DropdownMenuComponent from "./dropdownmenucomponent";
-import { deleteBook } from "@/lib/deleteBook";
+import {deleteBook} from "@/lib/deleteBook";
 import EditModal from "./editmodal";
 import LoadingBook from "./loadingBook";
 
 const Book = () => {
-  const [books, setBooks] = useState([]);
-  const [editBook, setEditBook] = useState<{
-    id: number;
-    title: string;
-    author: string;
-  } | null>(null);
-  const [isLoading, setLoading] = useState(true);
+    const [books, setBooks] = useState([]);
+    const [editBook, setEditBook] = useState<{
+        id: number;
+        title: string;
+        author: string;
+    } | null>(null);
+    const [isLoading, setLoading] = useState(true);
 
-  useEffect(() => {
-    setLoading(true);
-    async function getAllBooks() {
-      try {
-        const response = await fetch("http://localhost:8080/library/book/all", {
-          method: "GET",
-        });
+    useEffect(() => {
+        setLoading(true);
 
-        if (!response.ok) {
-          throw new Error("Failed to fetch all books.");
+        async function getAllBooks() {
+            try {
+                const response = await fetch("http://localhost:8080/library/book/all", {
+                    method: "GET",
+                });
+
+                if (!response.ok) {
+                    throw new Error("Failed to fetch all books.");
+                }
+                const data = await response.json();
+                setBooks(data);
+            } catch (error) {
+                console.error(error);
+            }
         }
-        const data = await response.json();
-        setBooks(data);
-      } catch (error) {
-        console.error(error);
-      }
-    }
-    getAllBooks();
-    setLoading(false);
-  }, []);
 
-  const handleDelete = async (bookId: number) => {
-    await deleteBook(bookId);
-    setBooks((prev) => prev.filter((book) => book.id !== bookId));
-  };
+        getAllBooks();
+        setLoading(false);
+    }, []);
 
-  const handleEdit = (
-    bookId: number,
-    bookTitle: string,
-    bookAuthor: string
-  ) => {
-    if (editBook) {
-      setEditBook(null);
-    } else {
-      setEditBook({ id: bookId, title: bookTitle, author: bookAuthor });
-    }
-  };
+    const handleDelete = async (bookId: number) => {
+        await deleteBook(bookId);
+        setBooks((prev) => prev.filter((book) => book.id !== bookId));
+    };
 
-  return (
-    <>
-      {books.map((book) => (
-        <div
-          key={book.id}
-          className="bg-surface-a10 w-72 min-h-[100%] max-w-[80%] min-w-[50%] rounded overflow-hidden p-3 h-fit 
+    const handleEdit = (
+        bookId: number,
+        bookTitle: string,
+        bookAuthor: string
+    ) => {
+        if (editBook) {
+            setEditBook(null);
+        } else {
+            setEditBook({id: bookId, title: bookTitle, author: bookAuthor});
+        }
+    };
+
+    return (
+        <>
+            {books.map((book) => (
+                <div
+                    key={book.id}
+                    className="bg-surface-a10 w-[60%] sm:w-[50%] md:w-[80%] rounded overflow-hidden p-3
           border-2 border-transparent hover:border-2 hover:border-primary-a0 transition duration-400 ease-in-out">
           <span className="flex justify-end mb-2">
             <DropdownMenuComponent
-              bookId={book.id}
-              bookTitle={book.title}
-              bookAuthor={book.author}
-              onDelete={handleDelete}
-              onEdit={handleEdit}
+                bookId={book.id}
+                bookTitle={book.title}
+                bookAuthor={book.author}
+                onDelete={handleDelete}
+                onEdit={handleEdit}
             />
           </span>
-          <div className="flex flex-col items-center text-center">
-            <div className="h-60 w-40 bg-surface-a20 col-span-1 mb-2"></div>
-            <p className="mb-2 font-bold">{book.title}</p>
-            <p className="text-sm">{book.author}</p>
-          </div>
-        </div>
-      ))}
-      {editBook && (
-        <EditModal
-          bookId={editBook.id}
-          bookTitle={editBook.title}
-          bookAuthor={editBook.author}
-          onClose={() => setEditBook(null)}
-        />
-      )}
-      {isLoading && (
-        <>
-          <LoadingBook />
-          <LoadingBook />
+                    <div className="flex flex-col items-center text-center">
+                        <div className="h-60 w-40 bg-surface-a20 col-span-1 mb-2"></div>
+                        <p className="mb-2 font-bold">{book.title}</p>
+                        <p className="text-sm">{book.author}</p>
+                    </div>
+                </div>
+            ))}
+            {editBook && (
+                <EditModal
+                    bookId={editBook.id}
+                    bookTitle={editBook.title}
+                    bookAuthor={editBook.author}
+                    onClose={() => setEditBook(null)}
+                />
+            )}
+            {isLoading && (
+                <>
+                    <LoadingBook/>
+                    <LoadingBook/>
+                </>
+            )}
         </>
-      )}
-    </>
-  );
+    );
 };
 
 export default Book;
--- frontend/app/(dashboard)/_components/loadingBook.tsx
+++ frontend/app/(dashboard)/_components/loadingBook.tsx
@@ -1,27 +1,27 @@
-import { Ellipsis } from "lucide-react";
+import {Ellipsis} from "lucide-react";
 
 const LoadingBook = () => {
-  return (
-    <div
-      className="bg-surface-a10 w-72 min-h-[100%] max-w-[80%] min-w-[50%] rounded overflow-hidden p-3 h-fit 
+    return (
+        <div
+            className="bg-surface-a10 md:w-[120%] w-[150%] rounded overflow-hidden p-3 h-fit
           border-2 border-transparent">
       <span className="flex animate-pulse justify-end mb-2 cursor-pointer">
-        <Ellipsis />
+        <Ellipsis/>
       </span>
-      <div className="flex flex-col justify-center items-center animate-pulse ">
-        <div className="h-60 w-40 bg-surface-a20 col-span-1 mb-2"></div>
-        <div className="flex-1 space-y-6 py-1">
-          <div className="h-2 w-40 rounded bg-gray-200"></div>
-          <div className="space-y-3">
-            <div className="grid grid-cols-1 gap-4">
-              <div className="col-span-2 h-2 rounded bg-gray-200"></div>
-              <div className="col-span-1 h-2 rounded bg-gray-200"></div>
+            <div className="flex flex-col justify-center items-center animate-pulse ">
+                <div className="h-60 w-40 bg-surface-a20 col-span-1 mb-2"></div>
+                <div className="flex-1 space-y-6 py-1">
+                    <div className="h-2 w-40 rounded bg-gray-200"></div>
+                    <div className="space-y-3">
+                        <div className="grid grid-cols-1 gap-4">
+                            <div className="col-span-2 h-2 rounded bg-gray-200"></div>
+                            <div className="col-span-1 h-2 rounded bg-gray-200"></div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default LoadingBook;
