90189e9a72fff199c250b46c9dc9913b7db74c6e
Matthew Garcia <gmatthew2765@gmail.com>
2025-05-16 20:16:10 -0700

    importing spring security

--- backend/src/main/java/com/booklibrary/book_library_api/security/SecurityConfig.java
+++ backend/src/main/java/com/booklibrary/book_library_api/security/SecurityConfig.java
@@ -1,4 +1,19 @@
 package com.booklibrary.book_library_api.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt());
+
+        return http.build();
+    }
 }
