8b81d6d0e3ea47a741831d92f917fc04e1be95e7
Matthew Garcia <gmatthew2765@gmail.com>
2025-05-19 21:29:19 -0700

    bug: fixed securit config import

--- backend/src/main/java/com/booklibrary/book_library_api/config/SecurityConfig.java
+++ backend/src/main/java/com/booklibrary/book_library_api/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import com.booklibrary.book_library_api.filter.JwtAuthFilter;
 
