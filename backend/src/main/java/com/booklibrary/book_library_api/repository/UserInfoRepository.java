5772ca3d0e3d8ff9487fef2340c7d80c53f08700
Matthew Garcia <gmatthew2765@gmail.com>
2025-05-19 19:02:47 -0700

    security: created user info repository

---
+++ backend/src/main/java/com/booklibrary/book_library_api/repository/UserInfoRepository.java
@@ -0,0 +1,12 @@
+package com.booklibrary.book_library_api.repository;
+
+import com.booklibrary.book_library_api.model.UserInfo;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Long>{
+  Optional<UserInfo> findByEmail(String email);
+}
