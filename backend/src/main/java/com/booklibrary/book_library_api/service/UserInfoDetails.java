1658f82b7b3a49e61270d3742abaf6a8303cd12a
Matthew Garcia <gmatthew2765@gmail.com>
2025-05-19 20:50:54 -0700

    security: implemented user info details service

---
+++ backend/src/main/java/com/booklibrary/book_library_api/service/UserInfoDetails.java
@@ -0,0 +1,56 @@
+package com.booklibrary.book_library_api.service;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.booklibrary.book_library_api.model.UserInfo;
+
+public class UserInfoDetails implements UserDetails {
+
+  private final String username;
+  private final String password;
+
+  public UserInfoDetails(UserInfo userInfo) {
+    this.username = userInfo.getUsername();
+    this.password = userInfo.getPassword();
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+  }
+
+  @Override
+  public String getPassword() {
+    return password;
+  }
+
+  @Override
+  public String getUsername() {
+    return username;
+  }
+
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
+}
